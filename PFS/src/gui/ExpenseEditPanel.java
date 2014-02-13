83f2ef9c22f9cdbbdc7ba69eb71d34dfe6697bb6
Aaron Vaage <aaron.vaage@gmail.com>
2014-02-12 23:30:25 -0600

    Started Expense Edit Panel

---
+++ PFS/src/gui/ExpenseEditPanel.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+public class ExpenseEditPanel extends JPanel
+{	
+	public ExpenseEditPanel() 
+	{
+		
+		JButton btnNewButton = new JButton("Add Labels");
+		add(btnNewButton);
+		
+		
+	}
+}
