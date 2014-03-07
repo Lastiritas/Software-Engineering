7495c5b28ecc31e706e2bfde398ef271db40ce3e
Lastiritas <yayo.lastiri89@gmail.com>
2014-03-07 13:55:30 -0600

    GUI - View Expense : Added menu bar
    Changed view expense to have a menu bar that holds the less used
    buttons to prevent cluttering of the GUI.
    
    (This does not work on OSX because of how it handles menus).

--- PFS/src/gui/ViewExpense.java
+++ PFS/src/gui/ViewExpense.java
@@ -7,9 +7,11 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 
 import domainobjects.Expense;
 import domainobjects.ExpenseFilter;
@@ -20,7 +22,7 @@ import domainobjects.SimpleDate;
 import system.ExpenseManagement;
 import system.PFSystem;
 import util.Sort;
-import org.eclipse.swt.graphics.Point;
+
 
 public class ViewExpense implements IWindow 
 {
@@ -28,9 +30,6 @@ public class ViewExpense implements IWindow
 	private Table expenseTable;
 	
 	protected int currID;
-	private Button btnFilter;
-	private Button btnViewMining;
-	private Button btnEditSelected;
 	
 	private int sCountID=1;
 	private int sCountDate =0;
@@ -66,54 +65,12 @@ public class ViewExpense implements IWindow
 	{
 		shell = new Shell();
 		shell.setMinimumSize(new Point(800, 600));
-		shell.setSize(968, 740);
+		shell.setSize(800, 600);
 		shell.setText("PayTo Creation");
 		
-		Composite composite_1 = new Composite(shell, SWT.NONE);
-		composite_1.setBounds(10, 10, 882, 631);
-		
-		Button deleteButton = new Button(composite_1, SWT.NONE);
-		deleteButton.addSelectionListener(new SelectionAdapter() 
-		{
-			@Override
-			public void widgetSelected(SelectionEvent arg0) 
-			{
-				if(expenseTable.getSelectionCount() == 0)
-				{
-					return;
-				}
-				
-				final int selectedIndex = expenseTable.getSelectionIndex();
-				final TableItem[] items = expenseTable.getSelection();
-				
-				final int id = Integer.parseInt(items[0].getText(0));
-				final boolean deleted = PFSystem.getCurrent().getExpenseSystem().delete(id);
-				
-				if(deleted)
-				{
-					expenseTable.remove(selectedIndex);
-				}
-			}
-		});
-		deleteButton.setBounds(578, 593, 94, 28);
-		deleteButton.setText("-");
-		
-		Button addButton = new Button(composite_1, SWT.NONE);
-		addButton.addSelectionListener(new SelectionAdapter() 
-		{
-			@Override
-			public void widgetSelected(SelectionEvent arg0) 
-			{
-				final int id = PFSystem.getCurrent().getExpenseSystem().create();
-				addExpense(id);
-				expenseTable.select(expenseTable.getItemCount() - 1);
-			}
-		});
-		addButton.setBounds(778, 593, 94, 28);
-		addButton.setText("+");
-		
-		expenseTable = new Table(composite_1, SWT.BORDER | SWT.FULL_SELECTION);
-		expenseTable.setBounds(10, 10, 862, 577);
+		expenseTable = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		expenseTable.setLocation(10, 10);
+		expenseTable.setSize(780, 524);
 		expenseTable.setHeaderVisible(true);
 		expenseTable.setLinesVisible(true);
 		
@@ -140,6 +97,152 @@ public class ViewExpense implements IWindow
 			}
 			column.setText (columnHeaders[i]);
 		}
+		
+		Menu menu = new Menu(shell, SWT.BAR);
+		menu.setLocation(new Point(0, 0));
+		shell.setMenuBar(menu);
+		
+		MenuItem mntmNewSubmenu = new MenuItem(menu, SWT.CASCADE);
+		mntmNewSubmenu.setText("File");
+		
+		Menu menu_1 = new Menu(mntmNewSubmenu);
+		mntmNewSubmenu.setMenu(menu_1);
+		
+		MenuItem mntmExit = new MenuItem(menu_1, SWT.NONE);
+		mntmExit.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				shell.close();
+			}
+		});
+		mntmExit.setText("Exit");
+		
+		MenuItem mntmFilter = new MenuItem(menu, SWT.CASCADE);
+		mntmFilter.setText("Filter");
+		
+		Menu menu_3 = new Menu(mntmFilter);
+		mntmFilter.setMenu(menu_3);
+		
+		MenuItem mntmCreateFilter = new MenuItem(menu_3, SWT.NONE);
+		mntmCreateFilter.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				IDialog filterDialog = new FilterCreation();
+				ExpenseFilter filter = (ExpenseFilter)filterDialog.open();
+				
+				if(filter != null)
+				{
+					refreshWholeList(filter);
+				}
+				// else - do nothing
+			}
+		});
+		mntmCreateFilter.setText("Create Filter");
+		
+		MenuItem mntmViewCommonLabels = new MenuItem(menu_3, SWT.NONE);
+		mntmViewCommonLabels.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				IDialog miningDialog = new MinedView();
+				ExpenseFilter filter = (ExpenseFilter)miningDialog.open();
+				
+				if(filter != null)
+				{
+					refreshWholeList(filter);
+				}
+				// else - do nothing
+			}
+		});
+		mntmViewCommonLabels.setText("View Common Labels");
+		
+		Button addButton = new Button(shell, SWT.NONE);
+		addButton.setBounds(210, 540, 94, 28);
+		addButton.addSelectionListener(new SelectionAdapter() 
+		{
+			@Override
+			public void widgetSelected(SelectionEvent arg0) 
+			{
+				final int id = PFSystem.getCurrent().getExpenseSystem().create();
+				addExpense(id);
+				expenseTable.select(expenseTable.getItemCount() - 1);
+			}
+		});
+		addButton.setText("+");
+		
+		Button duplicateButton = new Button(shell, SWT.NONE);
+		duplicateButton.setBounds(110, 540, 94, 28);
+		duplicateButton.addSelectionListener(new SelectionAdapter() 
+		{
+			@Override
+			public void widgetSelected(SelectionEvent arg0) 
+			{
+				if(expenseTable.getSelectionCount() == 0)
+				{
+					return;
+				}
+				
+				final TableItem[] items = expenseTable.getSelection();
+				
+				final int selectedID = Integer.parseInt(items[0].getText(0));
+				final Expense selectedExpense = (Expense)PFSystem.getCurrent().getExpenseSystem().getDataByID(selectedID);
+								
+				final int createdID = PFSystem.getCurrent().getExpenseSystem().create();
+				PFSystem.getCurrent().getExpenseSystem().update(createdID, selectedExpense);
+				
+				addExpense(createdID);
+								
+				expenseTable.select(expenseTable.getItemCount() - 1);
+			}
+		});
+		duplicateButton.setText("Duplicate");
+		
+		Button deleteButton = new Button(shell, SWT.NONE);
+		deleteButton.setBounds(10, 540, 94, 28);
+		deleteButton.addSelectionListener(new SelectionAdapter() 
+		{
+			@Override
+			public void widgetSelected(SelectionEvent arg0) 
+			{
+				if(expenseTable.getSelectionCount() == 0)
+				{
+					return;
+				}
+				
+				final int selectedIndex = expenseTable.getSelectionIndex();
+				final TableItem[] items = expenseTable.getSelection();
+				
+				final int id = Integer.parseInt(items[0].getText(0));
+				final boolean deleted = PFSystem.getCurrent().getExpenseSystem().delete(id);
+				
+				if(deleted)
+				{
+					expenseTable.remove(selectedIndex);
+				}
+			}
+		});
+		deleteButton.setText("-");
+		
+		Button btnOpen = new Button(shell, SWT.NONE);
+		btnOpen.setBounds(696, 540, 94, 28);
+		btnOpen.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				final int selectedExpenseIndex = expenseTable.getSelectionIndex();
+				
+				if(selectedExpenseIndex >= 0)
+				{
+					final int expenseId = Integer.parseInt(expenseTable.getItem(selectedExpenseIndex).getText(0));
+					
+					EditExpense editExpenseWindow = new EditExpense();
+					editExpenseWindow.setExpenseId(expenseId);
+					editExpenseWindow.open();
+					
+					updateExpenseForRow(selectedExpenseIndex, expenseId);
+				}
+			}
+		});
+		btnOpen.setText("Open");
+		
 		
 		expenseTable.getColumn(0).addSelectionListener(new SelectionAdapter()
 		{
@@ -271,98 +374,6 @@ public class ViewExpense implements IWindow
 				}
 			}
 		});
-				
-		Button duplicateButton = new Button(composite_1, SWT.NONE);
-		duplicateButton.addSelectionListener(new SelectionAdapter() 
-		{
-			@Override
-			public void widgetSelected(SelectionEvent arg0) 
-			{
-				if(expenseTable.getSelectionCount() == 0)
-				{
-					return;
-				}
-				
-				final TableItem[] items = expenseTable.getSelection();
-				
-				final int selectedID = Integer.parseInt(items[0].getText(0));
-				final Expense selectedExpense = (Expense)PFSystem.getCurrent().getExpenseSystem().getDataByID(selectedID);
-								
-				final int createdID = PFSystem.getCurrent().getExpenseSystem().create();
-				PFSystem.getCurrent().getExpenseSystem().update(createdID, selectedExpense);
-				
-				addExpense(createdID);
-								
-				expenseTable.select(expenseTable.getItemCount() - 1);
-			}
-		});
-		duplicateButton.setBounds(678, 593, 94, 28);
-		duplicateButton.setText("Duplicate");
-		
-		Button btnQuit = new Button(composite_1, SWT.NONE);
-		btnQuit.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent arg0) {
-				shell.close();
-			}
-		});
-		btnQuit.setBounds(10, 593, 94, 28);
-		btnQuit.setText("Quit");
-		
-		btnFilter = new Button(composite_1, SWT.NONE);
-		btnFilter.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent arg0) {
-				IDialog filterDialog = new FilterCreation();
-				ExpenseFilter filter = (ExpenseFilter)filterDialog.open();
-				
-				if(filter != null)
-				{
-					refreshWholeList(filter);
-				}
-				// else - do nothing
-			}
-		});
-		btnFilter.setText("Filter");
-		btnFilter.setBounds(110, 593, 94, 28);
-		
-		btnViewMining = new Button(composite_1, SWT.NONE);
-		btnViewMining.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent arg0) {
-				IDialog miningDialog = new MinedView();
-				ExpenseFilter filter = (ExpenseFilter)miningDialog.open();
-				
-				if(filter != null)
-				{
-					refreshWholeList(filter);
-				}
-				// else - do nothing
-			}
-		});
-		btnViewMining.setBounds(210, 593, 94, 28);
-		btnViewMining.setText("Labels");
-		
-		Button btnOpen = new Button(composite_1, SWT.NONE);
-		btnOpen.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent arg0) {
-				final int selectedExpenseIndex = expenseTable.getSelectionIndex();
-				
-				if(selectedExpenseIndex >= 0)
-				{
-					final int expenseId = Integer.parseInt(expenseTable.getItem(selectedExpenseIndex).getText(0));
-					
-					EditExpense editExpenseWindow = new EditExpense();
-					editExpenseWindow.setExpenseId(expenseId);
-					editExpenseWindow.open();
-					
-					updateExpenseForRow(selectedExpenseIndex, expenseId);
-				}
-			}
-		});
-		btnOpen.setBounds(394, 593, 94, 28);
-		btnOpen.setText("Open");
 		
 		refreshWholeList(new ExpenseFilter());
 	}
